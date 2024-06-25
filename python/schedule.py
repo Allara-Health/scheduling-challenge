050f35eb370c12fa51be390fd7ef303af4913305
Frances Hughes <francesbhughes@gmail.com>
2024-06-25 12:27:30 -0400

    Change python starter

--- python/schedule.py
+++ python/schedule.py
@@ -1,7 +1,18 @@
-def schedulingChallenge(filename):
+def read_schedule(filename):
+    schedule = {}
     with open(filename, 'r') as file:
+        next(file)
         for line in file:
-            print(line)
+            parts = line.strip().split(',')
+            provider_name = parts[0].strip()
+            start_time = parts[1].strip()
+            end_time = parts[2].strip()
+            schedule[provider_name] = (start_time, end_time)
+    return schedule
+
+def schedulingChallenge(filename):
+    schedule = read_schedule(filename)
+    print(schedule)
 
 
 filename = 'test.txt'
